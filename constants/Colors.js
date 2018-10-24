7b6dd1a71124adab75a050a3cc2f3bac13439983
ntpbright <nuttapatprom.c@ku.com>
2018-10-24 14:05:28 +0700

    Add header

---
+++ constants/Colors.js
@@ -0,0 +1,15 @@
+const tintColor = '#2f95dc';
+
+export default {
+  navBarColor: '#008BCC',
+  tintColor,
+  tabIconDefault: '#ccc',
+  tabIconSelected: tintColor,
+  tabBar: '#fefefe',
+  errorBackground: 'red',
+  errorText: '#fff',
+  warningBackground: '#EAEB5E',
+  warningText: '#666804',
+  noticeBackground: tintColor,
+  noticeText: '#fff',
+};
